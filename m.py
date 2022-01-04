5349137746d1c96e88cf08d43a1e4ea41da3ad06
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2022-01-04 10:22:16 +0530

    Add files via upload

---
+++ m.py
@@ -0,0 +1,43 @@
+import cv2
+import board
+import neopixel
+import numpy as p
+from time import sleep
+colors = p.random.randint(0, 255,(80, 3)) 
+np = neopixel.NeoPixel(board.D18, 60)
+np.brightness = 0.5
+    
+def RGB(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE :  
+        colorsBGR = img[y, x]
+        colorsRGB=tuple(reversed(colorsBGR))
+        
+        for i in range(60):     
+           np[i]=(colorsRGB)
+           sleep(0.01)
+        
+        
+image = cv2.imread("/home/pi/rgb.jpg")
+img = cv2.resize(image,(640,480))
+
+
+cv2.namedWindow('RGB')
+cv2.setMouseCallback('RGB', RGB)
+
+
+while True:
+    cv2.imshow('RGB', img)
+     
+    
+           
+    
+        
+         
+    if cv2.waitKey(10) & 0xFF == 27:
+        break
+      
+        
+for i in range(60):
+     np[i]=(0,0,0)
+    
+cv2.destroyAllWindows()
